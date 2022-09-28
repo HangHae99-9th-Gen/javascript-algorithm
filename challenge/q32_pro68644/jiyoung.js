7ca87421cba6a31916c5723951606ca2825867a5
chamchimayo <chamchi.maayo@gmail.com>
2022-09-29 01:25:29 +0900

    feat: 2022-09-29 68644 두 개 뽑아서 더하기

--- challenge/q32_pro68644/jiyoung.js
+++ challenge/q32_pro68644/jiyoung.js
@@ -6,16 +6,49 @@
 // 모든 수를 배열에 오름차순으로 담아 
 // return 하도록 solution 함수를 완성해주세요.
 
+// 2) 이중 for문
 function solution(numbers) {
     let answer = [];
-    numbers.forEach((item, index) => {
-        console.log("현재 요소: ", item)
-        console.log("다음 요소: ", numbers[numbers.indexOf(item)+1])
-        answer.push(item + numbers[numbers.indexOf(item)+1])
-    })
-    
-    return answer;
+
+    for(let i = 0; i < numbers.length; i++) {
+        for(let j = i+1; j < numbers.length; j++) {
+            if(answer.indexOf(numbers[i] + numbers[j])===-1){
+                answer.push(numbers[i] + numbers[j])
+            }
+        }
+    }
+
+    return answer.sort((a, b) => a - b)
 }
 
+
+// 1) 나 왜 이딴식으로 풀었지?
+// function solution(numbers) {
+//     let answer = [];
+//     numbers.forEach((item, index) => {
+//         for(let i = 0; i < numbers.length; i++) {
+//             if(i+index+1 < numbers.length) {
+//                 answer.push(item + numbers[i+index+1])
+//             }
+//         }
+//     })
+//     return answer.filter((element, index) => answer.indexOf(element) === index).sort((a, b) => a - b);
+// }
+
+// 3) 프로그래머스 코드
+// function solution(numbers) {
+//     const temp = []
+
+//     for (let i = 0; i < numbers.length; i++) {
+//         for (let j = i + 1; j < numbers.length; j++) {
+//             temp.push(numbers[i] + numbers[j])
+//         }
+//     }
+
+//     const answer = [...new Set(temp)]
+
+//     return answer.sort((a, b) => a - b)
+// }
+
 console.log(solution([2,1,3,4,1]))
-// console.log(solution([5,0,2,7]))
+console.log(solution([5,0,2,7]))
